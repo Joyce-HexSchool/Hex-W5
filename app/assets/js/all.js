3cbca6547ae51a37c254b204389dbf6d77ed69d8
joyce <joyce0723huang@gmail.com>
2022-09-13 23:35:20 +0800

    Update 2022-09-13T15:35:14.805Z

--- app/assets/js/all.js
+++
@@ -1,9 +0,0 @@
-// $(function() {
-//   console.log('Hello Bootstrap5');
-// });
-
-//index 右側 reply 按下彈出視窗 
-let replyCard = document.querySelector('#replyCard')
-    function showReply() {
-        replyCard.classList.toggle('d-none')
-    }
---
+++ assets/js/all.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+//index 右側 reply 按下彈出視窗 
+var replyCard = document.querySelector('#replyCard');
+
+function showReply() {
+  replyCard.classList.toggle('d-none');
+}
+//# sourceMappingURL=all.js.map
