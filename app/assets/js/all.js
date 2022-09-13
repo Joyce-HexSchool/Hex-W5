dc934e8ea57f4c2d585e2d1751188e14faafbd9e
joyce <joyce0723huang@gmail.com>
2022-09-13 23:12:05 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,9 @@
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+
+//index 右側 reply 按下彈出視窗 
+let replyCard = document.querySelector('#replyCard')
+    function showReply() {
+        replyCard.classList.toggle('d-none')
+    }
